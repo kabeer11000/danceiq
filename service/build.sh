95bd4f721c1748a2770c225bfbfd6f31946f76cf
kabeer11000 <kabeer@kabeersnetwork.tk>
2024-03-23 18:05:13 +0500

    initial commit

---
+++ service/build.sh
@@ -0,0 +1,3 @@
+cmake -B /Users/asadrizvi/Documents/assignments/danceiq/service/builds -S . "-DCMAKE_TOOLCHAIN_FILE=/Users/asadrizvi/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build /Users/asadrizvi/Documents/assignments/danceiq/service/builds
+#export VCPKG_ROOT="$HOME/vcpkg"   
---
+++ service/build/CMakeFiles/danceiq.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for danceiq.
+# This may be replaced when dependencies are built.
